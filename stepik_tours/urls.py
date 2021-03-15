20d6934c10f4490516c390ac6df7f28069128b60
OlgaBeznutrova <olga.beznutrova@gmail.com>
2021-03-15 18:06:16 +1200

    Corrected after 1st check

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -19,7 +19,7 @@ import tours.views as tours_views
 urlpatterns = [
     path("", tours_views.main_view),
     path("departure/<str:departure>/", tours_views.departure_view),
-    path("tour/<int:id>/", tours_views.tour_view),
+    path("tour/<int:tour_id>/", tours_views.tour_view),
 ]
 
 handler404 = tours_views.custom_handler404
--- tours/views.py
+++ tours/views.py
@@ -11,7 +11,7 @@ def departure_view(request, departure):
     return render(request, "tours/departure.html")
 
 
-def tour_view(request, id):
+def tour_view(request, tour_id):
     return render(request, "tours/tour.html")
 
 
